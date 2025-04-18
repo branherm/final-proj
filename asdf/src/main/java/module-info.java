b2d9b7f0270cd465b9c3d706111f2acb8a29ed58
Brandon <bhermes@guilford.edu>
2025-04-17 23:58:28 -0400

    changes

--- asdf/src/main/java/module-info.java
+++ asdf/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module edu.guilford {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;  // Add this line
 
     opens edu.guilford to javafx.fxml;
     exports edu.guilford;
